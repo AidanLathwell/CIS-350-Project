b7b198e623b477149bf5c88e9af54086fd51af8e
lmj11 <165704181+lmj11@users.noreply.github.com>
2024-10-01 11:52:53 -0400

    Add files via upload

---
+++ Deck.py
@@ -0,0 +1,51 @@
+from Card import Card
+import random
+
+class Deck:
+    def __init__(self):
+
+        # list variable that will hold the 312 cards in the current deck
+        self.deck = []
+
+    """ Method internally called by print message to represent list of cards. // chatgpt """
+    def __str__(self):
+        return "[" + ", ".join(str(card) for card in self.deck) + "]"
+
+    """ Method used to generate the original deck of cards when starting a blackjack game. 
+        The method loops 6 times to create 6 decks as most casinos use a 312 card deck. It 
+        then adds each card to the self.deck list after creation. """
+    def create_deck(self):
+
+        # for loop that will iterate 6 times
+        for deck in range(6):
+
+            # list containing the 4 types of suits
+            suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+            # list containing the 13 different card values
+            values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+
+            # iterate through each suit
+            for suit in suits:
+
+                # iterate though each value
+                for value in values:
+                    new_card = Card(suit, value)
+                    self.deck.append(new_card)
+
+    """ Method used to randomize the deck after reaching cut card or starting game. """
+    def shuffle_deck(self):
+        random.shuffle(self.deck)    # from python docs, built-in function to randomize sequence
+
+    def hit(self):
+        if len(self.deck) >= 0:
+            return self.deck.pop(0)
+        else:
+            return None
+
+
+# test = Deck()
+# test.create_deck()
+# print(test)
+# test.shuffle_deck()
+# print(test)    # after shuffle
