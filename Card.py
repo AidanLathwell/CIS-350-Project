a89d205866d3d1e108d6c6a4aee03488bc28772e
lmj11 <165704181+lmj11@users.noreply.github.com>
2024-10-01 12:19:57 -0400

    Add files via upload

--- Card.py
+++ Card.py
@@ -1,13 +1,12 @@
 class Card:
     def __init__(self, suit, value):
 
-        # string variable to represent the suit of the card
+        # String variable to represent the suit of the card
         self.suit = suit
 
-        # string variable to represent the value of the card
+        # String variable to represent the value of the card
         self.value = value
 
-    """ Method internally called by print message to represent list of cards in speicific
-        string format. """
+    """ Method internally called by print message to represent list of cards. // chatgpt """
     def __str__(self):
         return f"{self.value} of {self.suit}"
