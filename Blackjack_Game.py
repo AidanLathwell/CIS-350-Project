3d59cca7e6daed6c980f28331659fb177eac1af4
lmj11 <165704181+lmj11@users.noreply.github.com>
2024-10-01 11:58:25 -0400

    Add files via upload

---
+++ Blackjack_Game.py
@@ -0,0 +1,19 @@
+class Blackjack:
+    def __init__(self):
+        self.deck = Deck()
+        self.player = Player()
+        self.dealer = Dealer()
+        self.current_bet = 0
+
+    def start(self):
+        pass
+
+    def player_turn(self):
+        pass
+
+    def dealer_turn(self):
+        pass
+
+    def determine_winner(self):
+        pass
+
